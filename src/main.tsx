c874a5945460b9801299f75e74251cb0f2d73a58
RoaaK95 <ruaakhuldoon.26@gmail.com>
2025-09-22 13:35:28 +0300

    CONNECT APP TO ROUTER

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './router'
-import './styles/global.css'
-import App from './App'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import "./styles/global.css";
+import App from "./App";
 const qc = new QueryClient({
-  defaultOptions:{
-    queries:{
+  defaultOptions: {
+    queries: {
       staleTime: 60_000,
       refetchOnWindowFocus: false,
-     }
-  }
+    },
+  },
 });
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={qc}>
- <App />
+      <App />
     </QueryClientProvider>
-   </StrictMode>,
-)
+  </StrictMode>
+);
