a33cc30c3a9daf55f2377a1415b5fb53e1d73238
RoaaK95 <ruaakhuldoon.26@gmail.com>
2025-09-22 11:08:39 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+ 
+  return (
+    <>
+      <h1>Welcome to Clean Air Day</h1>
+      <p>Join us in promoting clean air initiatives.</p>
+    </>
+  )
+}
+
+export default App
